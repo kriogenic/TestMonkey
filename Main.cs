05ec5cf7f5f0ff605b17ed4868e328767da5e098
Kriogenic <monkey103@gmail.com>
2023-09-03 18:42:06 +1000

    Changed to use duplicate of upgraded towermodel instead of Tower

--- Main.cs
+++ Main.cs
@@ -46,7 +46,8 @@ namespace TestMonkey
                     MelonLogger.Msg("Tower in Range: " + theTower.towerModel.name);
 
                     MelonLogger.Msg("We have " + tower.rootModel.Cast<TowerModel>().GetBehavior<AttackModel>().weapons.Count + " weapons before upgrade");
-                    TowerModel newModel = WeaponHelper.ModifyWeapon(theTower, tower);
+                    TowerModel newModel = ModifyWeapon(theTower, newBaseTowerModel);
+                    newBaseTowerModel = newModel;
                     tower.UpdateRootModel(newModel);
                     MelonLogger.Msg("We added another attack and now have " + tower.rootModel.Cast<TowerModel>().GetBehavior<AttackModel>().weapons.Count);
                    
@@ -58,21 +59,17 @@ namespace TestMonkey
                 }
             }
         }
-    }
 
-    public static class WeaponHelper
-    {
-        public static TowerModel? ModifyWeapon(Tower takeFromTower, Tower giveToTower)
+        public TowerModel ModifyWeapon(Tower takeFromTower, TowerModel giveToTower)
         {
             //If we are a pilot, ace or upgraded dartling\boomerang don't take it
-            if (takeFromTower.towerModel.name.ToLower().Contains("helipilot")) return null;
-            if (takeFromTower.towerModel.name.ToLower().Contains("monkeyace")) return null;
-            if (Regex.IsMatch(takeFromTower.towerModel.name, "DartlingGunner-4..") || Regex.IsMatch(takeFromTower.towerModel.name, "DartlingGunner-5..") ||
-                Regex.IsMatch(takeFromTower.towerModel.name, "BoomerangMonkey-5..")) return null;
+            if (takeFromTower.towerModel.name.ToLower().Contains("helipilot") || takeFromTower.towerModel.name.ToLower().Contains("monkeyace") ||
+                Regex.IsMatch(takeFromTower.towerModel.name, "DartlingGunner-4..") || Regex.IsMatch(takeFromTower.towerModel.name, "DartlingGunner-5..") ||
+                Regex.IsMatch(takeFromTower.towerModel.name, "BoomerangMonkey-5..")) return giveToTower;
 
             try
             {
-                TowerModel giveToTowerModel = giveToTower.rootModel.Cast<TowerModel>().Duplicate();
+                TowerModel giveToTowerModel = giveToTower.Duplicate();//giveToTower.rootModel.Cast<TowerModel>().Duplicate();
                 AttackModel takeFromAttackModel = takeFromTower.rootModel.Cast<TowerModel>().GetAttackModel();
                 AttackModel giveToAttackModel = giveToTowerModel.GetBehavior<AttackModel>().Duplicate();
                 giveToTowerModel.RemoveBehavior<AttackModel>();
@@ -82,15 +79,49 @@ namespace TestMonkey
 
                 giveToTowerModel.AddBehavior(giveToAttackModel);
                 return giveToTowerModel;
-               
+
             }
             catch (Exception e)
             {
                 MelonLogger.Msg($"Failed to add {takeFromTower.towerModel.name} attacks!");
                 MelonLogger.BigError("[TestMonkey-FatalError]", e.Message);
-                return giveToTower.rootModel.Cast<TowerModel>();
+                return giveToTower;//.rootModel.Cast<TowerModel>();
             }
         }
 
     }
+
+    //public static class WeaponHelper
+    //{
+    //    public static TowerModel? ModifyWeapon(Tower takeFromTower, TowerModel giveToTower)
+    //    {
+    //        //If we are a pilot, ace or upgraded dartling\boomerang don't take it
+    //        if (takeFromTower.towerModel.name.ToLower().Contains("helipilot")) return null;
+    //        if (takeFromTower.towerModel.name.ToLower().Contains("monkeyace")) return null;
+    //        if (Regex.IsMatch(takeFromTower.towerModel.name, "DartlingGunner-4..") || Regex.IsMatch(takeFromTower.towerModel.name, "DartlingGunner-5..") ||
+    //            Regex.IsMatch(takeFromTower.towerModel.name, "BoomerangMonkey-5..")) return null;
+
+    //        try
+    //        {
+    //            TowerModel giveToTowerModel = giveToTower.rootModel.Cast<TowerModel>().Duplicate();
+    //            AttackModel takeFromAttackModel = takeFromTower.rootModel.Cast<TowerModel>().GetAttackModel();
+    //            AttackModel giveToAttackModel = giveToTowerModel.GetBehavior<AttackModel>().Duplicate();
+    //            giveToTowerModel.RemoveBehavior<AttackModel>();
+    //            //Duplicate the WeaponModel and add it to giveToTower weapons
+    //            foreach (WeaponModel WP in takeFromAttackModel.weapons)
+    //                giveToAttackModel.AddWeapon(WP.Duplicate());
+
+    //            giveToTowerModel.AddBehavior(giveToAttackModel);
+    //            return giveToTowerModel;
+               
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            MelonLogger.Msg($"Failed to add {takeFromTower.towerModel.name} attacks!");
+    //            MelonLogger.BigError("[TestMonkey-FatalError]", e.Message);
+    //            return giveToTower.rootModel.Cast<TowerModel>();
+    //        }
+    //    }
+
+    //}
 }
